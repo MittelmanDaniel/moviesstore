902ed2033e3050c6a84e611c205e296d8249eddc
SteveHawKim03 <donggeon01px2022@saschina.org>
2025-10-09 20:58:41 -0400

    rating

--- movies/models.py
+++ movies/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Avg
 
 class Movie(models.Model):
     id = models.AutoField(primary_key=True)
@@ -11,9 +12,16 @@ class Movie(models.Model):
     def __str__(self):
         return str(self.id) + ' - ' + self.name
 
+    @property
+    def average_rating(self):
+        # The fix is here: .filter(rating__gt=0)
+        # This will only average reviews where a rating has been submitted.
+        return self.review_set.filter(rating__gt=0).aggregate(Avg('rating'))['rating__avg']
+
 class Review(models.Model):
     id = models.AutoField(primary_key=True)
-    comment = models.CharField(max_length=255)
+    comment = models.CharField(max_length=255, blank=True, null=True)
+    rating = models.IntegerField(default=0)
     date = models.DateTimeField(auto_now_add=True)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
